ffb13fa617ef83b276f038c81a7a868818dff8b2
Lo-Deck <122597745+Lo-Deck@users.noreply.github.com>
2025-07-25 11:15:30 +0200

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 
 
@@ -20,19 +20,27 @@ MAIN () {
     MAIN "Please enter a name"
   else
 
-    NAME_USERS=$($PSQL "SELECT username FROM users WHERE username = '$USERNAME'")
+    NAME_USER=$($PSQL "SELECT username FROM users WHERE username = '$USERNAME'")
 
-    if [[ $USERNAME != $NAME_USERS ]]
+    # if [[ $USERNAME != $NAME_USERS ]]
+    if [[ -z $NAME_USER ]]
     then
-      echo -e "\nCreate a new player: "
-      read NEW_PLAYER
-
-      INSERT_NEW_PLAYER=$($PSQL "")
-
+      #create new player
+      echo -e "Welcome, $USERNAME! It looks like this is your first time here."
+      INSERT_NEW_PLAYER=$($PSQL "INSERT INTO users(username, games_played, best_game) VALUES ('$USERNAME', 0, 0)")
+      # GAMES_PLAYED=0
+      # BEST_GAME=0
     else
       #player already existing
-
+      USER_DATA=$($PSQL "SELECT username, games_played, best_game FROM users WHERE username = '$USERNAME'")
+      IFS='|' read USERNAME_FROM_DB GAMES_PLAYED BEST_GAME <<< "$USER_DATA" 
+      echo -e "Welcome back, '$USERNAME_FROM_DB'! You have played $GAME_PLAYED games, and your best game took $BEST_GAME guesses."
     fi
+
+    echo -e "Guess the secret number between 1 and 1000:"
+
+    
+
 
   fi
 
