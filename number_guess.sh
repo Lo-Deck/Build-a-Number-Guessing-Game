c6a68153f4d48f0848980872a657d59fdf4ad648
Lo-Deck <122597745+Lo-Deck@users.noreply.github.com>
2025-07-25 12:16:48 +0200

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -28,8 +28,8 @@ MAIN () {
       #create new player
       echo -e "Welcome, $USERNAME! It looks like this is your first time here."
       INSERT_NEW_PLAYER=$($PSQL "INSERT INTO users(username, games_played, best_game) VALUES ('$USERNAME', 0, 0)")
-      # GAMES_PLAYED=0
-      # BEST_GAME=0
+      GAMES_PLAYED=0
+      BEST_GAME=0
     else
       #player already existing
       USER_DATA=$($PSQL "SELECT username, games_played, best_game FROM users WHERE username = '$USERNAME'")
@@ -40,52 +40,62 @@ MAIN () {
 
     echo -e "Guess the secret number between 1 and 1000:"
 
-    RANDOM_NUMBER=(( RANDOM % 1000 + 1 ))
+    RANDOM_NUMBER=$(( RANDOM % 1000 + 1 ))
 
-    GUESS_NUMBER
+
+    USER_NUMBER=$(GUESS_NUMBER)
+
 
     NUMBER_TRIES=0
 
 
-    while $USER_NUMBER != $RANDOM_NUMBER
+    while (( USER_NUMBER != RANDOM_NUMBER ))
     do
 
-      $NUMBER_TRIES++
+      (( NUMBER_TRIES++ ))
 
       if [[ $USER_NUMBER < $RANDOM_NUMBER ]]
       then
-        GUESS_NUMBER "It's lower than that, guess again: "
+        USER_NUMBER=$(GUESS_NUMBER "It's lower than that, guess again: ")
       fi
 
       if [[ $USER_NUMBER > $RANDOM_NUMBER ]]
       then
-        GUESS_NUMBER "It's higher than that, guess again: "
-      fi
-
-      if [[ $USER_NUMBER == $RANDOM_NUMBER ]]
-      then
-        $GAME_PLAYED++
-
-        if [[ $NUMBER_TRIES < $BEST_GAME ]]
-        then
-          BEST_GAME=$NUMBER_TRIES
-        fi
-
-        INSERT_NEW_DATA=$($PSQL "INSERT INTO users(games_played, best_game) VALUES ($GAME_PLAYED, $NUMBER_TRIES)")
-        echo "You guessed it in $NUMBER_TRIES tries. The secret number was $USER_NUMBER. Nice job!"
-
+        USER_NUMBER=$(GUESS_NUMBER "It's higher than that, guess again: ")
       fi
 
     done
+
+    if [[ $USER_NUMBER == $RANDOM_NUMBER ]]
+    then
+
+      (( GAME_PLAYED++ ))
+
+      if [[ $NUMBER_TRIES < $BEST_GAME ]]
+      then
+        BEST_GAME=$NUMBER_TRIES
+      fi
+
+      # INSERT_NEW_DATA=$($PSQL "INSERT INTO users(games_played, best_game) VALUES ($GAME_PLAYED, $NUMBER_TRIES)")
+      INSERT_NEW_DATA=$($PSQL "UPDATE users SET games_played = $GAMES_PLAYED, best_game = $NUMBER_TRIES WHERE username = '$USERNAME'")
+      
+      echo "You guessed it in $NUMBER_TRIES tries. The secret number was $USER_NUMBER. Nice job!"
+
+    fi
+
 
   fi
 
 
 }
 
-MAIN
 
 GUESS_NUMBER () {
+
+  if [[ $1 ]]
+  then
+    echo $1
+  fi
 
   read USER_NUMBER
 
@@ -95,11 +105,10 @@ GUESS_NUMBER () {
     GUESS_NUMBER "That is not an integer, guess again:"
   fi
 
-  # echo -e "Guess the secret number between 1 and 1000:"
-  # RANDOM_NUMBER=(( RANDOM % 1000 + 1 ))
-  # read USER_NUMBER
-  # if [[ $USER_NUMBER != $RANDOM_NUMBER ]]
-  # then
-  # fi
+  echo "$USER_NUMBER"
 
 }
+
+
+MAIN
+
